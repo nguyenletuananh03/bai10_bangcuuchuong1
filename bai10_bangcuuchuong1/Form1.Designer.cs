4f6e43b2b641ae0dbc9d11a751a0451b5fa1ccae
ADMIN <ADMIN@32-LAB01>
2025-02-17 16:49:22 +0700

    Add project files.

---
+++ bai10_bangcuuchuong1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace bai10_bangcuuchuong1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnKQ = new System.Windows.Forms.Button();
+            this.txtso = new System.Windows.Forms.TextBox();
+            this.lsbBCC = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(331, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "bảng cửu chương";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "nhập số ";
+            // 
+            // btnKQ
+            // 
+            this.btnKQ.Location = new System.Drawing.Point(455, 88);
+            this.btnKQ.Name = "btnKQ";
+            this.btnKQ.Size = new System.Drawing.Size(75, 23);
+            this.btnKQ.TabIndex = 1;
+            this.btnKQ.Text = "KQ";
+            this.btnKQ.UseVisualStyleBackColor = true;
+            this.btnKQ.Click += new System.EventHandler(this.btnKQ_Click);
+            // 
+            // txtso
+            // 
+            this.txtso.Location = new System.Drawing.Point(322, 88);
+            this.txtso.Name = "txtso";
+            this.txtso.Size = new System.Drawing.Size(100, 20);
+            this.txtso.TabIndex = 2;
+            // 
+            // lsbBCC
+            // 
+            this.lsbBCC.FormattingEnabled = true;
+            this.lsbBCC.Location = new System.Drawing.Point(302, 152);
+            this.lsbBCC.Name = "lsbBCC";
+            this.lsbBCC.Size = new System.Drawing.Size(172, 95);
+            this.lsbBCC.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lsbBCC);
+            this.Controls.Add(this.txtso);
+            this.Controls.Add(this.btnKQ);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnKQ;
+        private System.Windows.Forms.TextBox txtso;
+        private System.Windows.Forms.ListBox lsbBCC;
+    }
+}
+
---
+++ bai10_bangcuuchuong1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai10_bangcuuchuong1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKQ_Click(object sender, EventArgs e)
+        {
+            lsbBCC.Items.Clear();
+            int so = Convert.ToInt32(txtso.Text);
+            for (int i = 1; i <= 10; i++)
+            {
+                lsbBCC.Items.Add(so + " x " + i + " = " + so * i);
+            }
+        }
+    }
+}
